9df5afc3d3808f4064022945966a1612348ff87c
Evan <Donoyster@icloud.com>
2024-09-25 15:17:26 -0700

    I completed scripting assignment
    I created my first scene and completed my first scripting assignment  I was able to add 5 more colors than required :muscle: :computer:

---
+++ ScriptingProjectP8Perez/Assets/Scripts/ColorChanger.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.RendererUtils;
+
+public class ColorChanger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            GetComponent<Renderer>().material.color = Color.red;
+        }
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            GetComponent<Renderer>().material.color = Color.green;
+        }
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            GetComponent<Renderer>().material.color = Color.blue;
+        }
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            GetComponent<Renderer>().material.color = Color.clear;
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            GetComponent<Renderer>().material.color = Color.white;
+        }
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            GetComponent<Renderer>().material.color = Color.gray;
+        }
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            GetComponent<Renderer>().material.color = Color.magenta;
+        }
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            GetComponent<Renderer>().material.color = Color.yellow;
+        }
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            GetComponent<Renderer>().material.color = Color.cyan;
+        }
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            GetComponent<Renderer>().material.color = Color.black;
+        }
+   //I just got 10 colors
+    }
+}
+
+
