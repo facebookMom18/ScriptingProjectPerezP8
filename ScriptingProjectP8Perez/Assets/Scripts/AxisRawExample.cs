ef23af59c9b548a7198098c94ddb42cc6d87f4e1
Evan <Donoyster@icloud.com>
2025-03-10 09:05:19 -0700

    sd
    sd

---
+++ ScriptingProjectP8Perez/Assets/Scripts/AxisRawExample.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AxisRawExample : MonoBehaviour
+{
+    public float range;
+ 
+
+
+    void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float xPos = h * range;
+
+        transform.position = new Vector3(xPos, 2f, 0);
+ 
+    }
+}
